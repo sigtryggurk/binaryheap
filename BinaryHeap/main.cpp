c3bcc84b7aaa66bc8a3f5bc56a0c957a6d3fe29c
Sigtryggur <sigtryggur@sigtryggur.com>
2015-05-14 23:32:47 -0700

    Initial Commit

---
+++ BinaryHeap/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BinaryHeap
+//
+//  Created by Sigtryggur Kjartansson on 5/14/15.
+//  Copyright (c) 2015 Sigtryggur Kjartansson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
